b716ae4f77c1d3573162a86dc6b8250f77a17a98
Jose Ramirez <hoyeay5@gmail.com>
2016-12-07 05:38:54 +0000

    Added contact form mailer file

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,15 @@
+class ContactMailer < ActionMailer::Base
+   
+    defaul to: 'hoyeay5@gmail.com'
+    
+    def contact_email(name, email, body)
+       
+        @name = name
+        @email = email
+        @body = body
+        
+        mail(from: email, subject: 'Contact Form Message')
+        
+    end
+    
+end
